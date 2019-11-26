6dde93d57f7f07afb65f8caf88e5a4b836591173
Alexey <valnistuy@gmail.com>
2019-11-27 01:26:36 +0200

    added hw-js-04

---
+++ js/task-02.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+const inventory = {
+  items: ["Монорельса", "Фильтр"],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  }
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction("Аптечка", inventory.add.bind(inventory));
+// inventory.add("Аптечка");
+// Invoking action on Аптечка
+// Adding Аптечка to inventory
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryAction("Фильтр", inventory.remove.bind(inventory));
+// inventory.remove("Фильтр");
+// Invoking action on Фильтр
+// Removing Фильтр from inventory
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
